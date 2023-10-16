fea24b230732253124c256672a8b9049e40d2f0a
Julien Ginestal <144034157+Dju0@users.noreply.github.com>
2023-10-16 15:47:39 +0200

    twig quest 3 done

--- config/twig.php
+++ config/twig.php
@@ -6,3 +6,5 @@ $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
 
+$contactEmail = 'juju@gin.fr';
+$twig->addGlobal('contactEmail', $contactEmail);
---
+++ public/details.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/../config/twig.php';
+
+// Affiche la vue
+echo $twig->render('details.html.twig');
